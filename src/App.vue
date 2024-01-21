81685fe37b8f6a3dc87499352fd847b15af9522b
tamistyping <enquiretam@gmail.com>
2024-01-21 10:55:39 +0000

    tailwind add on

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <div>
-    <routerView />
+    <SiteNavigation />
+    <RouterView />
   </div>
 </template>
 
 <script setup>
 import { RouterView } from 'vue-router';
+import SiteNavigation from "./components/SiteNavigation.vue"
 </script>
 
 <style lang="scss" scoped>
